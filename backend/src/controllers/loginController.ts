b6b0969d46c986d79cde686deb9763cfee3a5422
guilhermex2 <guilhermexavier522@gmail.com>
2026-04-19 10:26:49 -0300

    Login por coordenador ou agente

--- backend/src/controllers/loginController.ts
+++ backend/src/controllers/loginController.ts
@@ -1,4 +1,3 @@
-// src/controllers/auth.controller.ts
 import { prisma } from "../libs/prisma";
 import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
@@ -6,18 +5,30 @@ import jwt from "jsonwebtoken";
 export async function login(req: Request, res: Response) {
   const { nome, senha } = req.body;
 
-  console.log(req.body)
-  const agente = await prisma.agente.findUnique({ where: { nome } });
+  const agente = await prisma.agente.findUnique({
+    where: { nome }
+  });
 
-  if (!agente || agente.password !== senha) { // usar bcrypt em produção
+  if (!agente || agente.password !== senha) {
     return res.status(401).json({ erro: "Credenciais inválidas" });
   }
 
   const token = jwt.sign(
-    { agenteId: agente.id, nome: agente.nome }, // payload
+    {
+      agenteId: agente.id,
+      nome: agente.nome,
+      role: agente.role // 🔥 aqui está a mudança importante
+    },
     process.env.JWT_SECRET!,
     { expiresIn: "1h" }
   );
 
-  res.json({ token });
+  res.json({
+    token,
+    usuario: {
+      id: agente.id,
+      nome: agente.nome,
+      role: agente.role
+    }
+  });
 }
