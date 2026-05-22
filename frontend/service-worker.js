af86aece78f9fd9f1954db308df50e66d2382d78
guilhermex2 <guilhermexavier522@gmail.com>
2026-05-21 21:36:09 -0300

    Fix: Sw e manifest

--- frontend/service-worker.js
+++ frontend/service-worker.js
@@ -1,28 +1,28 @@
 const CACHE_NAME = "sisav-v2";
 
 const FILES_TO_CACHE = [
-  "/sisav/frontend/html/login.html",
-  "/sisav/frontend/html/turno.html",
-  "/sisav/frontend/html/ficha-registro.html",
-  "/sisav/frontend/html/historico.html",
-  "/sisav/frontend/html/pendentes.html",
-  "/sisav/frontend/html/ficha-recuperacao.html",
-  "/sisav/frontend/html/tabela-recuperacao.html",
-  "/sisav/frontend/html/tabela-registros.html",
-  "/sisav/frontend/html/resumo_campo.html",
-  "/sisav/frontend/css/style.css",
-  "/sisav/frontend/js/db.js",
-  "/sisav/frontend/js/historico.js",
-  "/sisav/frontend/js/recuperacao.js",
-  "/sisav/frontend/js/registros-new.js",
-  "/sisav/frontend/js/resumo-campo.js",
-  "/sisav/frontend/js/tabela-recuperacao.js",
-  "/sisav/frontend/js/turno-guard.js",
-  "/sisav/frontend/js/turno.js",
-  "/sisav/frontend/js/sync.js",
-  "/sisav/frontend/manifest.json",        
-  "/sisav/frontend/icons/icon-192.png",
-  "/sisav/frontend/icons/icon-512.png"
+  "/html/login.html",
+  "/html/turno.html",
+  "/html/ficha-registro.html",
+  "/html/historico.html",
+  "/html/pendentes.html",
+  "/html/ficha-recuperacao.html",
+  "/html/tabela-recuperacao.html",
+  "/html/tabela-registros.html",
+  "/html/resumo_campo.html",
+  "/css/style.css",
+  "/js/db.js",
+  "/js/historico.js",
+  "/js/recuperacao.js",
+  "/js/registros-new.js",
+  "/js/resumo-campo.js",
+  "/js/tabela-recuperacao.js",
+  "/js/turno-guard.js",
+  "/js/turno.js",
+  "/js/sync.js",
+  "/manifest.json",        
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
 ]
 
 self.addEventListener("install", event => {
@@ -74,7 +74,7 @@ self.addEventListener("fetch", event => {
             if (cachedResponse) return cachedResponse;
 
             if (event.request.mode === "navigate") {
-              return caches.match("/sisav/frontend/html/login.html"); // 
+              return caches.match("/html/login.html"); // 
             }
           });
       })
